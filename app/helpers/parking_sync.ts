c2d28d4dacecc19916e3a4bc84628d3b2243b9b8
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2026-03-02 22:28:07 +0100

    fix: format ISO datetimes into time strings

--- app/helpers/parking_sync.ts
+++ app/helpers/parking_sync.ts
@@ -1,3 +1,6 @@
+import { nullableMap } from "@solvro/utils/option";
+import { DateTime } from "luxon";
+
 import type { CarPark } from "#helpers/iparking_api";
 import Parking from "#models/parking";
 
@@ -11,6 +14,13 @@ export function parseTrend(trend: string): number {
   return trendMap[trend] ?? 0;
 }
 
+function isoTsToTime(iso: string): string {
+  return DateTime.fromISO(iso)
+    .setZone("Europe/Warsaw")
+    .setLocale("pl-PL")
+    .toLocaleString(DateTime.TIME_24_SIMPLE);
+}
+
 export async function upsertMetadataParking(
   carPark: CarPark,
 ): Promise<Parking> {
@@ -18,39 +28,25 @@ export async function upsertMetadataParking(
 
   parking ??= await Parking.findBy("external_id", carPark.id);
 
-  if (parking !== null) {
-    await parking
-      .merge({
-        symbol: carPark.symbol,
-        externalId: carPark.id,
-        name: carPark.name,
-        access: carPark.access,
-        closeHour: carPark.closeHour,
-        openHour: carPark.openHour,
-        places: carPark.totalSlots,
-        geoLan: Number(carPark.geoLan),
-        geoLat: Number(carPark.geoLat),
-        address: carPark.address,
-        isActive: true,
-        isVisible: true,
-      })
-      .save();
-    return parking;
-  }
-
-  parking = await Parking.create({
+  const parkingData = {
     symbol: carPark.symbol,
     externalId: carPark.id,
     name: carPark.name,
     access: carPark.access,
-    closeHour: carPark.closeHour,
-    openHour: carPark.openHour,
+    closeHour: nullableMap(carPark.closeHour, isoTsToTime),
+    openHour: nullableMap(carPark.openHour, isoTsToTime),
     places: carPark.totalSlots,
     geoLan: Number(carPark.geoLan),
     geoLat: Number(carPark.geoLat),
     address: carPark.address,
     isActive: true,
     isVisible: true,
-  });
+  };
+  if (parking !== null) {
+    await parking.merge(parkingData).save();
+    return parking;
+  }
+
+  parking = await Parking.create(parkingData);
   return parking;
 }
