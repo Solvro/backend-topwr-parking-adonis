5c4c38e2ccb66a1d4b3d426198233043b78eea95
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2026-05-26 19:46:38 +0200

    feat: healthcheck routes

--- start/routes.ts
+++ start/routes.ts
@@ -2,6 +2,14 @@ import router from "@adonisjs/core/services/router";
 
 const ParkingsController = () => import("#controllers/parkings_controller");
 
+
+router.get("/", async () => {
+  return { elo: "żelo" };
+});
+router.get("/health", async () => {
+  return { elo: "żelo" };
+});
+
 router
   .group(() => {
     router.get("/", [ParkingsController, "index"]);
